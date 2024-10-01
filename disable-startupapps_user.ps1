e9a56c398e359e8856d4a286ba44615eb292ebf2
Kris Turk <kris.turk@gitch.solutions>
2024-10-01 15:47:01 +1300

    added disable startup apps script

---
+++ disable-startupapps_user.ps1
@@ -0,0 +1,17 @@
+$keys = @("HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\StartupApproved\Run", "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\StartupApproved\StartupFolder")
+
+$appsToDisable = @("Greenshot","Redirector")
+
+# Binary Value for Disabled
+$binaryValue = [byte[]](3, 0, 0, 0, 135, 159, 80, 94, 164, 19, 219, 1)
+
+
+foreach($app in $appsToDisable){
+    foreach($key in $keys){
+        $appKey = Get-ItemProperty -Path $key -Name $app -ErrorAction SilentlyContinue
+        if($appKey -ne $null){
+            Write-Host "Disabling $app"
+            Set-ItemProperty -Path $key -Name $app -Value $binaryValue
+        }
+    }
+}
